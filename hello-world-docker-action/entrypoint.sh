f122e7ff707feb8e8ca1e390c02d99b364855a3e
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-09-18 18:42:29 +0530

    Update entrypoint.sh

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/sh -l
+# #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+# echo "Hello $1"
+# time=$(date)
+# echo "time=$time" >> $GITHUB_OUTPUT
+
+
+#!/bin/sh
+
+# Read the contents of the 'example.txt' file and print it
+file_contents=$(cat example.txt)
+
+# Print the file contents
+echo "Contents of example.txt:"
+echo "$file_contents"
