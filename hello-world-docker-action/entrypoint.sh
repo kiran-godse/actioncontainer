89e39bebb310061624e9ff916a0205a1dbcd74b9
Kiran P Godse <kpgodse@gmail.com>
2023-09-18 18:53:00 +0530

    updated the files

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -7,9 +7,10 @@
 
 #!/bin/sh
 
-# Read the contents of the 'example.txt' file and print it
-file_contents=$(cat example.txt)
+# Read the contents of the 'src/example.txt' file and print it
+file_contents=$(cat /mnt/src/example.txt)
 
 # Print the file contents
-echo "Contents of example.txt:"
+echo "Contents of src/example.txt:"
 echo "$file_contents"
+
